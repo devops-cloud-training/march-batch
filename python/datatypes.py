ca73ee53068660ba9d97bc23ae4ca4bb4d1679ac
devops-cloud-training <sathishdevopstrainer@gmail.com>
2024-05-09 07:36:56 +0530

    flow control

--- python/datatypes.py
+++ python/datatypes.py
@@ -29,28 +29,77 @@
 
 # lists  -are mutable
 
-fruits = ["apple", "orange", "grapes", "banana", "litchi"]
-sequences = [1,2,3,4,5,6,7,8,9]
-myownvalues = ["sathish","abc",2,3,4,5,6,"xyz"]
+# fruits = ["apple", "orange", "grapes", "banana", "litchi"]
+# sequences = [1,2,3,4,5,6,7,8,9]
+# myownvalues = ["sathish","abc",2,3,4,5,6,"xyz"]
 
-print(len(fruits))
-fruits.append("pomo")
-print(fruits)
-fruits.insert(1,"mango")
-print(fruits)
-fruits.remove("orange")
-print(fruits)
+# print(len(fruits))
+# fruits.append("pomo")
+# print(fruits)
+# fruits.insert(1,"mango")
+# print(fruits)
+# fruits.remove("orange")
+# print(fruits)
 
-myownvalues.extend(fruits)
-print(myownvalues)
-fruits.reverse()
-print(fruits)
+# myownvalues.extend(fruits)
+# print(myownvalues)
+# fruits.reverse()
+# print(fruits)
 
-myownvalues.sort()
-print(myownvalues)
+# myownvalues.sort()
+# print(myownvalues)
 
 # tuples
 # sets
 # dictionary
 
-a = 5
+# sequences = (1,2,3,4,5,6,7,8,9,2,3,4,3,3,43,3,2)
+# print(len(sequences))
+# print(sequences.count(3))
+# print(sequences[::-1])
+
+# Sets
+
+myset = {1,2,3,4,5,6,7,8,9,6,5,6,4,5,6,7,4,3,5,6,3,2,2,4,3,5,6,7,8,9,7,6,5,4,3}
+
+newset = {4,5,7,8,432,24,23,1,4,323,54,65,76}
+
+print(type(myset))
+
+# myset.add(12)
+# myset.add(1322)
+# myset.add(1422)
+# myset.discard(6)
+
+# print(myset.union(newset))
+# print(myset.intersection(newset))
+# print(myset.difference(newset))
+
+# print(myset)
+
+#dict
+
+employee = {
+    "name" : "sathish",
+    "hobbies": ["music","cricket","football","cooking"],
+    "age" : 90,
+    "salary": 9000000000,
+    "IsMarried": True,
+    "city" : "Chennai"
+}
+
+eatables = {
+    "fruits": ["apple","orange","banana"],
+    "veggies": "capsicum",
+    "sweet" : ["kesari","laddu"]
+}
+
+print(type(employee))
+
+emptyset = set()
+
+print(type(emptyset))
+print(emptyset)
+
+emptyset.add(3)
+print(emptyset)
---
+++ python/flowcontrol.py
@@ -0,0 +1,43 @@
+# If else conditions 
+a = 20
+b = 10
+c = a-b
+
+print(a)
+print(b)
+print(c)
+
+# print("The value is Zero")
+# print("The value is positive")
+# print("The value is negative")
+
+# if c == 0:
+#     print("The value is Zero")
+# elif c > 0:
+#     print("The value is positive")
+#     print("this is additional line")
+#     if c > 10:
+#         print("the number is greater than 10")
+#     elif c < 10:
+#         print("The number is less than 10")
+#     else:
+#         print("the number is equals to 10")
+# else:
+#     print("The value is negative")
+
+# print("this is new line")
+
+# looping - for , while
+
+sequences = [1,2,3,4,5,6,7,8,9,324,43,25,345,34,23,4,234,423,5,43,6,45,43,5,23]
+
+for mynum in sequences:
+    if mynum == 4:
+        print(mynum)
+    
+# print only the numbers which are divided by 2
+# print only the odd numbers
+# print only the prime numbers
+# print all the alternate numbers
+# print if the number is even but not divisible by 8
+# print if the number is divisible by 5 and its quotient as well
